f3c24450009f44aa735ae555894b2ecea2834aa3
Santiago Velandia <santivelandia2@hotmail.com>
2021-06-23 23:22:52 -0500

    Section 13 - Client side login and registration

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,15 +1,21 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
 import CommonStore from "./commonStore";
+import ModalStore from "./modalStore";
+import userStore from "./userStore";
 
 interface Store{
     activityStore: ActivityStore;
     commonStore: CommonStore;
+    userStore: userStore;
+    modalStore: ModalStore;
 }
 
 export const store: Store = {
     activityStore: new ActivityStore(),
-    commonStore: new CommonStore()
+    commonStore: new CommonStore(),
+    userStore: new userStore(),
+    modalStore: new ModalStore()
 }
 
 export const StoreContext = createContext(store);
