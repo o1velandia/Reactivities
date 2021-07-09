8dc215a78779ea916d119071094a6ee0e29707ea
Santiago Velandia <santivelandia2@hotmail.com>
2021-07-08 22:29:21 -0500

    Section 15 Client attendance feature

--- client-app/src/features/activites/form/ActivityForm.tsx
+++ client-app/src/features/activites/form/ActivityForm.tsx
@@ -12,24 +12,15 @@ import MyTextArea from '../../../app/common/form/MyTextArea';
 import { categoryOptions } from '../../../app/common/options/categoryOptions';
 import MySelectInput from '../../../app/common/form/MySelectInput';
 import MyDateInput from '../../../app/common/form/MyDateInput';
-import { Activity } from '../../../app/models/Activity';
+import { ActivityFormValues } from '../../../app/models/Activity';
 import { v4 as uuid } from 'uuid';
 
 export default observer (function ActivityForm(){
     const history = useHistory();
     const {activityStore} = useStore();
-    const {createActivity,updateActivity,
-            loading, loadActivity, loadingInitial} = activityStore;
+    const {createActivity,updateActivity, loadActivity, loadingInitial} = activityStore;
     const {id} = useParams<{id: string}>(); 
-    const [activity, setActivity] = useState<Activity>({
-        id:'',
-        title:'',
-        category:'',
-        description:'',
-        date: null,
-        city:'',
-        venue:''
-    });
+    const [activity, setActivity] = useState<ActivityFormValues>(new ActivityFormValues());
 
     const validationSchema = Yup.object({
         title: Yup.string().required('The activity title is required'),
@@ -41,12 +32,12 @@ export default observer (function ActivityForm(){
     })
 
     useEffect(()=>{
-        if(id) loadActivity(id).then(activity => setActivity(activity!));
+        if(id) loadActivity(id).then(activity => setActivity(new ActivityFormValues(activity)));
     },[id, loadActivity]);
 
 
-    function handleFormSubmit(activity: Activity) {
-        if (activity.id.length === 0) {
+    function handleFormSubmit(activity: ActivityFormValues) {
+        if (!activity.id) {
             let newActivity = {
                 ...activity,
                 id: uuid()
@@ -85,7 +76,7 @@ export default observer (function ActivityForm(){
                     <MyTextInput placeholder='Venue' name='venue'/>
                     <Button 
                         disabled={isSubmitting || !dirty || !isValid}
-                        loading={(loading)} 
+                        loading={(isSubmitting)} 
                         floated='right' 
                         positive 
                         type='submit' 
